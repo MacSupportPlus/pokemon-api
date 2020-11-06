ca636bcdce2ca78186439e752cc0ab9db6191122
MacSupportPlus <71103657+MacSupportPlus@users.noreply.github.com>
2020-11-06 15:45:31 -0600

    components, components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import './App.css';
 import {useState, useEffect} from 'react';
+import axios from 'axios';
 import PokemonCard from './components/PokemonCard';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function App() {
 
 
 
   const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=807")
+      .then(res => setResults([...results,res.data]))
+      .catch(err => console.log(err))
+
+
+  }, [])
 
   const getPokemonResults = () => {
     fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
